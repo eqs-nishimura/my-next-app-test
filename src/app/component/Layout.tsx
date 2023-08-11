5ae43e72a6b40008635a97e7f534c8b493485d6d
Nishimura Kensuke <67882223+eqs-nishimura@users.noreply.github.com>
2023-08-11 19:27:15 +0900

    test

---
+++ src/app/component/Layout.tsx
@@ -0,0 +1,16 @@
+import Header from '../component/Header'
+import Footer from '../component/Footer'
+ 
+export default function Layout({ }) {
+  return (
+    <>
+      <Header />
+      <main>
+      <div>
+        test
+      </div>
+    </main>
+      <Footer />
+    </>
+  )
+}
